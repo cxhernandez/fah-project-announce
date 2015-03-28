11407f2e195426fc1df106f7d9a610c89e01abed
cxhernandez <cxh@stanford.edu>
2015-03-28 03:48:48 -0700

    improve

--- psummary-rss.py
+++ psummary-rss.py
@@ -5,6 +5,11 @@ import pytz
 from datetime import datetime
 from feedgen.feed import FeedGenerator
 
+def loadJSON(file):
+	return json.load(open(file,'rb'))
+
+JSONFILE='/home/server/announcements/psummary.json'
+
 fg = FeedGenerator()
 fg.title('New FAH Beta Projects')
 fg.link( href='http://web.stanford.edu/~cxh/fah-beta.xml', rel='self' )
@@ -13,16 +18,17 @@ fg.description('A list of the most recent beta Projects on Folding@home')
 fg.id('http://web.stanford.edu/~cxh/fah-beta.xml')
 fg.language('en')
 
-old_projects = json.load(open('/home/server/announcements/psummary.json','rb'))
-new_projects = json.load(open('/home/server/announcements/psummary.json.tmp','rb'))
+old_projects = loadJSON(JSONFILE) 
+new_projects = loadJSON(JSONFILE + '.tmp') 
 old = [project['id'] for project in old_projects] 
 for project in new_projects:
 	if project['beta'] and project['id'] not in old:
 		fe = fg.add_entry()
-		fe.id('proj%s' % str(project['id']))
-		fe.title('Project %s' % str(project['id']))
+		pid = str(project['id'])
+		fe.id('proj%s' % pid)
+		fe.title('Project %s' % pid)
 		fe.link({'href': 'http://assign.stanford.edu/api/project/summary'})
 		fe.published(datetime.now(pytz.utc))
-		fe.description('@%s has started project %s. Tune into channel #%s00 for more information.' % (project['contact'], str(project['id']), str(project['id'])[:-2]))
+		fe.description('@%s has started project %s. Tune into channel #%s00 for more information.' % (project['contact'], pid, pid[:-2]))
 
 print fg.rss_str(pretty=True)
