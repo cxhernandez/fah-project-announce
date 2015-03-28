19ecbf055c7e9f95935ea8a130b6d2a599ab5a73
cxhernandez <cxh@stanford.edu>
2015-03-28 03:11:34 -0700

    add scripts

---
+++ post-announcements.sh
@@ -0,0 +1,32 @@
+################################################################################
+# THIS SCRIPT GENERATES AN RSS FEED WITH THE LATEST BETA PROJECTS ADDED TO FAH
+# THIS SCRIPT IS INTENDED TO NOTIFY INTERNAL USERS OF NEW PROJECTS VIA FAH SLACK
+################################################################################
+#!/bin/bash
+
+URI=http://assign.stanford.edu/api/project/summary #FAH JSON API
+TMP=/home/server/announcements/psummary.json.tmp #PATH JSON STRING DOWNLOAD (TEMPORARY)
+JSON=/home/server/announcements/psummary.json #PATH TO LOCAL VERSION OF JSON STRING
+FEED=/home/server/announcements/fah-beta.xml #PATH TO UP-TO-DATE VERSION OF RSS FEED
+
+#JSONSIZE=$(wc -c "$JSON" | cut -f 1 -d ' ') #GET SIZE OF LOCAL JSON STRING
+
+wget -q -N -T 10 -t 1 --output-document=$TMP $URI #DOWNLOAD JSON STRING FROM AS
+
+TMPSIZE=$(wc -c "$TMP" | cut -f 1 -d ' ') #GET SIZE OF DOWNLOADED JSON STRING
+
+DIFF=`cmp $JSON $TMP` #CHECK FOR DIFFERENCES IN FILE
+
+#IF DOWNLOAD IS NOT EMPTY AND THERE ARE DIFFERENCES THEN UPDATE RSS FEED
+if [ "$TMPSIZE" -gt 0  ] && [ -n "$DIFF" ]; then 
+	mv $FEED ${FEED}.old #DEPRECATE PREVIOUS VERSION OF RSS FEED
+	python /home/server/announcements/psummary-rss.py | head -n -3> $FEED #GENERATE NEW RSS FEED ITEMS
+	grep -A6 -m 4 "<item>" ${FEED}.old >> $FEED #INCLUDE 4 OF THE PREVIOUS RSS ITEMS (THIS IS NECESSARY FOR IFTTT TO RECOGNIZE CHANGES)
+	echo -e "\t</channel>" >> $FEED #END CHANNEL
+	echo "</rss>" >> $FEED #END RSS FEED
+
+	scp $FEED cxh@corn.stanford.edu:~/WWW/ #UPLOAD TO CORN
+	mv $TMP $JSON #UPDATE LOCAL VERSION OF JSON STRING
+else 
+	rm $TMP #DELETE USELESS VERSION OF JSON STRING
+fi
---
+++ psummary-rss.py
@@ -0,0 +1,29 @@
+# This script identifies new FAH projects using the AS JSON STRING API
+# and then generates an RSS feed to notify FAH Slack users.
+import json
+import pytz
+from datetime import datetime
+from feedgen.feed import FeedGenerator
+
+fg = FeedGenerator()
+fg.title('New FAH Beta Projects')
+fg.link( href='http://web.stanford.edu/~cxh/fah-beta.xml', rel='self' )
+fg.author( {'name':'cxh','email':'cxh@stanford.edu'} )
+fg.description('A list of the most recent beta Ppojects on Folding@home')
+fg.id('http://web.stanford.edu/~cxh/fah-beta.xml')
+fg.language('en')
+
+old_projects = json.load(open('/home/server/announcements/psummary.json','rb'))
+new_projects = projects = json.load(open('/home/server/announcements/psummary.json.tmp','rb'))
+old = [project['id'] for project in old_projects] 
+for project in new_projects:
+	if project['beta'] and project['id'] not in old:
+		fe = fg.add_entry()
+		fe.id('proj' + str(project['id']))
+		fe.title('Project ' + str(project['id']))
+		fe.link({'href': 'http://assign.stanford.edu/api/project/summary'})
+		fe.published(datetime.now(pytz.utc))
+		fe.description(project['contact'] + ' has started project ' + str(project['id']) + ". Tune into channel #" + str(project['id'])[:-2] + "00 for more information.")
+
+#fg.rss_file('/home/server/announcements/fah-beta.xml')
+print fg.rss_str(pretty=True)
